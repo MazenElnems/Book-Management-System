f13bc3518defecc348917464a3a24d22f9f90977
Mazen Elnems <mazenelnems22@gmail.com>
2025-12-24 15:57:55 +0200

    add JWT configurations
    - add JWT configurations to appsettings.development.json
    - create JWTOptions type to map these configurations
    - configure JWTOptions in service collection

---
+++ src/BMS.API/Options/JWTOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace BMS.API.Options;
+
+public class JWTOptions
+{
+    public string Key { get; set; }
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+    public double DurationInMinutes { get; set; }
+}
