67a4ec1583b9864a563e359240dd37e5b7243210
Mazen Elnems <mazenelnems22@gmail.com>
2025-12-24 15:55:18 +0200

    Add Identity and add identity tables in migration
    - extend the IdentityUser with ApplicationUser
    - add Identity Tables Migration and Update the DB Schema
    - add the Identity services and configurations to service collection in program.cs
    - add entityframework options to service collection in pogram.cs

---
+++ src/BMS.API/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BMS.API.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BMS.API.Data;
+
+public class AppDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options):
+        base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+
+}
--- src/BMS.API/Program.cs
+++ src/BMS.API/Program.cs
@@ -1,11 +1,28 @@
+using BMS.API.Data;
+using BMS.API.Data.Entities;
+using BMS.API.Options;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.Configure<JWTOptions>(builder.Configuration.GetSection("JWT"));
+
 builder.Services.AddControllers();
 
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>()
+    .AddEntityFrameworkStores<AppDbContext>();
 
 var app = builder.Build();
 
