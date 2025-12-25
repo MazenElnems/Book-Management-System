c02c8e711f4c67eecf3ca0d03535a39373dcdb79
Mazen Elnems <mazenelnems22@gmail.com>
2025-12-25 18:23:01 +0200

    seed the default Roles

--- src/BMS.API/Data/AppDbContext.cs
+++ src/BMS.API/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using BMS.API.Data.Entities;
+﻿using BMS.API.Constants;
+using BMS.API.Data.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,38 @@ public class AppDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid
         base(options)
     {
         
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+
+        optionsBuilder.UseSeeding(async (db, _) =>
+        {
+            if (db.Set<IdentityRole<Guid>>().FirstOrDefault(r => r.Name == ApplicationRoles.Admin) is null)
+            {
+                db.Set<IdentityRole<Guid>>().Add(new IdentityRole<Guid> { Name = ApplicationRoles.Admin });
+                db.SaveChanges();
+            }
+            if (db.Set<IdentityRole<Guid>>().FirstOrDefault(r => r.Name == ApplicationRoles.User) is null)
+            {
+                db.Set<IdentityRole<Guid>>().Add(new IdentityRole<Guid> { Name = ApplicationRoles.User });
+                db.SaveChanges();
+            }
+        })
+            .UseAsyncSeeding(async (db, _, cancellationToken) =>
+            {
+                if (await db.Set<IdentityRole<Guid>>().FirstOrDefaultAsync(r => r.Name == ApplicationRoles.Admin) is null)
+                {
+                    db.Set<IdentityRole<Guid>>().Add(new IdentityRole<Guid> { Name = ApplicationRoles.Admin });
+                    await db.SaveChangesAsync();
+                }
+                if (await db.Set<IdentityRole<Guid>>().FirstOrDefaultAsync(r => r.Name == ApplicationRoles.User) is null)
+                {
+                    db.Set<IdentityRole<Guid>>().Add(new IdentityRole<Guid> { Name = ApplicationRoles.User });
+                    await db.SaveChangesAsync();
+                }
+            });
     }
 
     protected override void OnModelCreating(ModelBuilder builder)
